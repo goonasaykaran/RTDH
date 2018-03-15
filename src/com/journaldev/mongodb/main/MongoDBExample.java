20572dee1a9af7750d193c62a1710ff20466ce08
Gunasekaran P <goonasaykaran@yahoo.co.in>
2018-03-15 13:58:29 +0800

    Added connection set up

---
+++ src/com/journaldev/mongodb/main/MongoDBExample.java
@@ -0,0 +1,64 @@
+package com.journaldev.mongodb.main;
+
+import java.net.UnknownHostException;
+
+import com.journaldev.mongodb.model.User;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.WriteResult;
+
+public class MongoDBExample {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        User user = createUser();
+        DBObject doc = createDBObject(user);
+
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("RTDH");
+        DBCollection col = db.getCollection("CASA");
+
+        //create user
+        WriteResult result = col.insert(doc);
+        System.out.println(result.getUpsertedId());
+        System.out.println(result.getN());
+        System.out.println(result.isUpdateOfExisting());
+        System.out.println(result.getLastConcern());
+
+        //read example
+        DBObject query = BasicDBObjectBuilder.start().add("_id", user.getId()).get();
+        DBCursor cursor = col.find(query);
+        while(cursor.hasNext()){
+            System.out.println(cursor.next());
+        }
+
+        //close resources
+        mongo.close();
+    }
+
+    private static DBObject createDBObject(User user) {
+        BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+
+        docBuilder.append("_id", user.getId());
+        docBuilder.append("name", user.getName());
+        docBuilder.append("role", user.getRole());
+        docBuilder.append("isEmployee", user.isEmployee());
+        return docBuilder.get();
+    }
+
+    private static User createUser() {
+        User u = new User();
+        u.setId(3);
+        u.setName("Arun");
+        u.setEmployee(true);
+        u.setRole("CEO");
+        return u;
+    }
+
+
+
+}
